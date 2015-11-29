b55f6cb663fe718cf350973e18627062a28065d9
sks <sks@sksdemacbook-pro-2.local>
2015-11-29 10:28:45 +0800

    Initial Commit

---
+++ aa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aa
+//
+//  Created by sks on 15/11/29.
+//  Copyright © 2015年 sks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aa
+//
+//  Created by sks on 15/11/29.
+//  Copyright © 2015年 sks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
